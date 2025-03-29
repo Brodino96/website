8ec588ed5b8c856da66610296d2ffd258ab4b34f
Vincent Acanfora <brodino96@gmail.com>
2025-03-29 17:54:13 +0100

    feat: 404 message is language bound

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,18 +1,19 @@
 export const languages = {
-    en: 'English',
-    it: 'Italian',
+    en: "English",
+    it: "Italian",
 };
 
-export const defaultLang = 'en';
+export const defaultLang = "en";
 
 export const ui = {
     en: {
-        'nav.home': 'Home',
-        'nav.about': 'About',
-        'nav.twitter': 'Twitter',
+        "nav.home": "Home",
+        "nav.about": "About",
+        "404.message": "The page you're looking for couldn't be found."
     },
     it: {
-        'nav.home': 'Home',
-        'nav.about': 'About',
+        "nav.home": "Home",
+        "nav.about": "About",
+        "404.message": "Impossibile trovare la pagina che stavi cercando"
     },
 } as const;
